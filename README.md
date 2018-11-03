188f6f29185d24bcbf82a584e1f32e3ef52c7926
René Ghosh <rene_ghosh@yahoo.com>
2018-11-03 21:26:56 +0100

    readme

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # SteMeL
 
-SteMeL is a music notation format inspired by step sequencers and MML. Its purpose is to 
-store a musical score in format that's both human readable and usable in code.
+A music notation format inspired by step sequencers and [MML](https://en.wikipedia.org/wiki/Music_Macro_Language).
+
+Its purpose is to store a musical score in format that's both human readable and usable in code.
+
+## SteMeL by example
+
+```
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|0|-|0|-|0|-|0|-|0|-|0|-|0|-|0|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+```
+
+You'd write: `0,,0,,0,,0,,0,,0,,0,,0,`
--- python/stemel.py
+++ python/stemel.py
@@ -3,7 +3,7 @@ num_chars = ['-','0','1','2','3','4','5','6','7','8','9', '.']
 int_chars = ['-','0','1','2','3','4','5','6','7','8','9']
 positive_numchars = ['0','1','2','3','4','5','6','7','8','9', '.']
 def parse(note_str):
-    note_line = note_str.strip()
+    note_line = note_str.strip()+" "
     base_frequency = 0.0
     amp_multiplier = 1.1
     bpm = 120
@@ -84,10 +84,10 @@ def parse(note_str):
             time_segment = 1.0/float(note_line[cursor+1:cursor2])
             # print("time segment assignment: %f" % time_segment)
             cursor=cursor2+1
-        elif command in positive_numchars:
+        elif command in num_chars:
             # adding a note
             cursor2=cursor
-            while (cursor2<length) and (note_line[cursor2] in positive_numchars):
+            while (cursor2<length) and (note_line[cursor2] in num_chars):
                 cursor2+=1
             note_value = float(note_line[cursor:cursor2])
             notes_list.append(note_value)
@@ -108,11 +108,11 @@ def parse(note_str):
                 duration += int(note_line[cursor:cursor2])-1
             cursor = cursor2
             sus_list.append(duration)
-        elif command == '+':
+        elif command == '>':
             # increasing amplitude
             amplitude = 2
             cursor2=cursor+1
-            while (cursor2<length) and (note_line[cursor2] == '+'):
+            while (cursor2<length) and (note_line[cursor2] == '>'):
                 cursor2 += 1
                 amplitude += 1
             cursor = cursor2
@@ -123,11 +123,11 @@ def parse(note_str):
                 amplitude += int(note_line[cursor:cursor2])-1
             cursor = cursor2
             amp_list.append(amplitude)
-        elif command == '-':
+        elif command == '<':
             # decreasing amplitude
             amplitude = 0
             cursor2=cursor+1
-            while (cursor2<length) and (note_line[cursor2] == '-'):
+            while (cursor2<length) and (note_line[cursor2] == '<'):
                 cursor2 += 1
                 amplitude -= 1
             cursor = cursor2
@@ -147,3 +147,49 @@ def parse(note_str):
         else:
             cursor += 1
     return (notes_array, dur_array, sus_array, amp_array)
+
+    
+from FoxDot.lib import *
+Scale.default="major"
+Clock.bpm=100
+Master().room=0.5
+Master().mix=0.3
+def sendto(ins, str):
+    (notes_array, dur_array, sus_array, amp_array)=parse(str)
+    ins.pitch = notes_array
+    ins.sus = sus_array
+    ins.dur=dur_array
+    ins.amp=amp_array
+
+b1 >> bass(lpf=160, formant=9)
+sendto(b1,"f0 l2 0>>,0/-7/7<<,0>>,0<</7")
+
+sendto(b1,"f0 l2 0/0,7>>>,0,4/0>>>/14")
+
+b1.solo(0)
+
+p1 >> pluck(shape=0, drive=0.4, bpf=1200, formant=3)
+sendto(p1,"f0 l2 ,0<</7_++,0__+/7<<<<")
+
+sendto(p1,"f0 l2 7_4/0_8,,12,,7,5,3,5_,,7_3,-12,-10,0")
+
+p1.solo()
+
+d1 >> play("x-o-", sample=1)
+
+d1 >> play("xxo[--]", sample=1)
+
+d1 >> play("-X[--]X", sample=1)
+
+d1 >> play("[-xxx]", sample=1)
+
+d1.solo()
+
+d1.solo()
+
+
+p2 >> ambi(chop=0, oct=3)
+sendto(p2,"0/4/7_____,4,4/0,4/0,4")
+
+sendto(p2,"5_,,3_,,0_8,,,,,,,")
+p2.solo()
