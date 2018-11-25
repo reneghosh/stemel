fbc5cd4b2d09ec5f0692794a97075ec9be59b98a
René Ghosh <rene_ghosh@yahoo.com>
2018-11-25 18:37:39 +0100

    release stuff

--- sonicpi/example.rb
+++ sonicpi/example.rb
@@ -15,28 +15,36 @@ def notes(note_s)
     note.strip!
     note.downcase!
     case note
-    when '-'
-      lookback_counter = 0
-      while (!vals[lookback_counter] && (vals[lookback_counter].value>0))
-        lookback_counter -=1
+    when /^-+?/
+      (0..note.size()-1).each do
+        lookback_counter = 0
+        while (!vals[lookback_counter] && (vals[lookback_counter].value>0))
+          lookback_counter -=1
+        end
+        if lookback_counter>=0
+          vals[lookback_counter].duration+=1
+        end
+        vals[counter]=Struct::Note::new(-1, 1, amplitude, 0)
+        counter += 1
       end
-      if lookback_counter>=0
-        vals[lookback_counter].duration+=1
+    when /^\*+?$/
+      (0..note.size()-1).each do
+        vals[counter]=Struct::Note::new(-1, 1, amplitude, 0)
+        counter += 1
       end
-      vals[counter]=Struct::Note::new(-1, 1, amplitude, 0)
-      counter += 1
-    when '*'
-      vals[counter]=Struct::Note::new(-1, 1, amplitude, 0)
-      counter += 1
     when '/'
       counter=0
       poly += 1
       vals = []
       polyvals[poly]=vals
-    when '>'
-      oct += 1
-    when '<'
-      oct -=1
+    when /^>+?/
+      (0..note.size()-1).each do
+        oct +=1
+      end
+    when /^<+?/
+      (0..note.size()-1).each do
+        oct -=1
+      end
       if oct < 0
         oct = 0
       end
@@ -60,13 +68,16 @@ def play_notes(note_s, step_size=1, shift=0)
   polys = the_notes.size()
   ended = false
   counter = 0
+  note_counter = 0
+  player = nil
   while !ended
     ended = true
     the_notes.each do |line|
       if polycursor < line.size()
         note = line[polycursor]
         if (note.value>0)
-          play note.value+shift, release:note.duration*step_size, amp:note.amplitude
+          player = play note.value+shift, sustain:note.duration*step_size*0.5, amp:note.amplitude, release:note.duration*step_size*0.25
+          note_counter += 1
         end
         if polycursor + 1 < line.size()
           ended = false
@@ -83,26 +94,26 @@ end
 
 live_loop :tick do
     cue :tick
-    sleep 0.25
+    sleep 0.125/8
 end
 
 
-in_thread(name: :lead1) do
-  loop do
-  sync :tick
-    with_fx :echo, mix: 0.3, phase: 0.0, amp: 0.4 do
-      use_synth :beep
-      play_notes "> 5 7 0 5 / 0 - - - 0 - - -", step_size=0.25, shift=0
-    end
-  end
-end
+# live_loop :lead1 do
+#   loop do
+#   sync :tick
+#     with_fx :echo, mix: 0.3, phase: 0.0, amp: 0.8 do
+#       use_synth :beep
+#       play_notes "> 5 7 0 5 / 0 --- 0 ---", step_size=0.25, shift=0
+#     end
+#   end
+# end
 
 
-p1 = "> > * 0 0 < 7 12 12 12 / > 7 7 7 * * *"
+p1 = "> 5 4 5 4 5 5 7 0 "
 live_loop :lead2 do
   sync :tick
-    with_fx :reverb, mix: 0.1, amp: 0.2 do
-      use_synth :beep
+    with_fx :reverb, mix: 0.1, amp: 0.4 do
+      use_synth :pluck
       play_notes p1, step_size=0.25, shift=0
     end
 end
@@ -112,6 +123,6 @@ live_loop :bass do
   sync :tick
   with_fx :reverb, mix: 0.3, phase: 0.25 do
     use_synth :fm
-    play_notes "> 0 - 0 ", step_size=0.25, shift=0
+    play_notes "> 0 - 0 0 ", step_size=0.125, shift=0
   end
 end
