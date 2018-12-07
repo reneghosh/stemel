8c6ae579015cf1dee4caf174f7ce56be6a6afcfc
René Ghosh <rene_ghosh@yahoo.com>
2018-12-07 23:34:13 +0100

    sonic py example

---
+++ ruby/lib/stemel.rb
@@ -0,0 +1,92 @@
+def make_notes(pattern_string)
+  polyvals=[]
+  vals = []
+  polyvals << vals
+  oct = 3
+  amplitude = 1.0
+  pattern_string.split(" ").each do |note|
+    note.strip!
+    note.downcase!
+    case note
+    when /^-+?/
+      note.size().times do
+        lookback_counter = vals.size()-1
+        while ((lookback_counter > 0) && (vals[lookback_counter].value<0))
+          lookback_counter -=1
+        end
+        if lookback_counter>=0
+          vals[lookback_counter][:duration]+=1
+          vals[lookback_counter][:sustain]+=1
+        end
+        vals << {:frequency => -1, :duration=> 1, :sustain => 1}
+      end
+    when /^\*+?$/
+      # rest
+      note.size().times do
+        vals << {:frequency => -1, :duration=> {"rest":1}, :sustain => 1}
+      end
+    when '/'
+      vals = []
+      polyvals << vals
+    when /^>+?/
+      note.size().times do
+        oct +=1
+      end
+    when /^<+?/
+      note.size().times do
+        oct -=1
+      end
+      if oct < 0
+        oct = 0
+      end
+    when /^[\d | \.]+?$/
+        vals << { :frequency => note.to_f+(oct*12), :duration=> 1, :sustain => 1}
+    else
+      puts "? " + note
+    end
+  end
+  polyvals
+end
+
+def separate(pattern)
+  durations = []
+  sustains = []
+  pitches = []
+  pattern.each do |line|
+    dur_buff =[]
+    sus_buff =[]
+    pit_buff =[]
+    line.each do |note|
+      dur_buff << note[:duration]
+      sus_buff << note[:sustain]
+      pit_buff << note[:frequency]
+    end
+    durations << dur_buff
+    sustains << sus_buff
+    pitches << pit_buff
+  end
+  [pitches, durations, sustains]
+end
+
+def make_pattern(score)
+  pattern = []
+  notes = make_notes(score)
+  return if notes.size()==0
+  length = notes.max{ |a,b| a.size() <=> b.size()}.size()
+  counter = 0
+  length.times do
+    seq_buffer = []
+    notes.each do |voice_line|
+      if (voice_line.size()>0)
+        seq_buffer << voice_line[counter % voice_line.size()]
+      end
+    end
+    pattern << seq_buffer
+    counter += 1
+  end
+  pattern = separate(pattern)
+  pattern
+end
+
+plan = make_pattern("0 - 5 8 / 5 * 5 5")
+puts plan.to_s
