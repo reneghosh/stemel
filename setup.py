42549a43a27d8ebbbe89158df78f35bcc87048df
René Ghosh <rene_ghosh@yahoo.com>
2018-12-20 01:06:09 +0100

    setup modified for v 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stemel",
-    version="0.0.1",
+    version="0.0.2",
     author="René Ghosh",
     author_email="rene.ghosh@gmail.com",
     description="stemel polyphonic patterns language",
